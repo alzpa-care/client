ac497c668becc8906f35fa1a7b4f741fb09b01ea
Kim JinWoo <kimjinu2540@gmail.com>
2024-01-19 10:34:13 +0900

    feat: label태그 설정 및 SVG 컴포넌트화 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  //SVG-컴포넌트로-사용하기(webpack)
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
