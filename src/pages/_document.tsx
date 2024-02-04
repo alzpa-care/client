dab5fc3c0713dd29c26384efaae11d725e2192da
Kim JinWoo <kimjinu2540@gmail.com>
2024-02-05 07:19:41 +0900

    fix: 배포에 필요한 document 설정 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head />
       <meta property="og:title" content="알파케어" />
+      <meta
+        property="og:url"
+        content="alzpa-care-client.vercel.app"
+      />
       <meta
         property="og:description"
         content="알츠하이머와 파킨슨 환자를 위한 지원과 공유의 플랫폼"
